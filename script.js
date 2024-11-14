ecd8af5233f1e22ba8eab1ce765e01a190c425b2
Ramz898 <ramziddin537@gmail.com>
2024-11-14 23:17:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let yoz = ('Sizda hozir random onlar chiqishni boshlanadi')
+alert(yoz)
+let num = ('Random orqali chiqqan son:22')
+alert(num)
+let ran = ('Random orgali chiqqan son: 22 edi biz uni 2 ga kopaytib 44 sonini chigardik, va shu sonni 2 ga bolib: 11 sonini chiqardik, va shu sonni 3 ga bolgandagi goldia; 1 sonini chigardik')
+
+
+alert(ran)
